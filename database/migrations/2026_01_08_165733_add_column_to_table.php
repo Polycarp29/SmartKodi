a746394e371049d1f6fd508f2d0ca49c88636807
Polycarp29 <fb.admin87@protonmail.com>
2026-01-08 20:06:39 +0300

    More changes

--- database/migrations/2026_01_08_165733_add_column_to_table.php
+++ database/migrations/2026_01_08_165733_add_column_to_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             //
             $table->softDeletes();
-            $table->timestamp('deleted_at')->nullable();
+            // $table->timestamp('deleted_at')->nullable();
         });
     }
 
