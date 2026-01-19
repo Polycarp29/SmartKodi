2c045a857312a196ef1ebb34ea6222a937f846fb
Polycarp29 <fb.admin87@protonmail.com>
2026-01-19 22:55:25 +0300

    Changes for rent

---
+++ database/migrations/2026_01_19_194848_create_penalties_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Data\Units;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penalties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Units::class);
+            $table->float('percentage')->nullable();
+            $table->timestamp('activated_at');
+            $table->timestamp('end_date');
+            $table->boolean('activate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penalties');
+    }
+};
