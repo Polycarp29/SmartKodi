0cfca428ca94211c884c41de0c88dd91d2d2b1b3
Polycarp29 <fb.admin87@protonmail.com>
2026-01-20 09:15:56 +0300

    More changes

--- app/Models/SystemConfigurations/Authorizations/Permissions.php
+++ app/Models/SystemConfigurations/Authorizations/Permissions.php
@@ -29,7 +29,7 @@ class Permissions extends Model
 
     // relationships
 
-    public function rolespermissions()
+    public function roles()
     {
         return $this->belongsToMany(Roles::class, 'roles_permissions', 'permissions_id', 'roles_id');
     }
--- app/Models/SystemConfigurations/Authorizations/Roles.php
+++ app/Models/SystemConfigurations/Authorizations/Roles.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Roles extends Model
 {
     //
+
+    protected $fillabe = [
+        'name',
+        'uuid',
+        'role_description',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permissions::class, 'roles_permissions', 'roles_id', 'permissions_id');
+    }
 }
